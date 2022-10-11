7039d3c7489fb89287bfde89f6604c51d7ab9d08
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-10-11 12:08:42 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,21 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if ((i + j) % 2 == 0){
+        fill(150, 150, 150);
+        rect(size * i, size * j, 25, 25);
+        if(j < 3){
+          fill(255, 0, 0);
+          ellipse(size * i + 12.5, size * j + 12.5, size - 5);
+        }
+        else {
+          if(j > 5){
+            fill(0, 0, 0);
+            ellipse(size * i + 12.5, size * j + 12.5, size - 5);
+          }
+        }
+      }
+      else {}
     }
   }
 }
