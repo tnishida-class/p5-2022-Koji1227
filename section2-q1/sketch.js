040f99bc7fc94072ce81b5d6c15c7bd6938a3bb1
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-10-11 11:31:54 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
+  createCanvas(200, 200);
+  fill(0);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    stroke(0, 0, 255);
+    ellipse(100, 100, 10 * i + 10)
+    if(i >= 5){
+      stroke(255, 0, 0);
+      ellipse(100, 100, 10 * i + 10);
+    }
   }
 }
