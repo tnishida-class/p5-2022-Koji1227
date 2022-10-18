0672bde5fac48a5974ed037db5a1633c8ec26a9a
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-10-18 12:07:21 +0900

    Chenge2 section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,19 +1,27 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let speed;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  speed = 0;
 }
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + 1 + speed) % cycle;
   // BLANK[1]
   let size;
   if(count < 50){size = count + 50;}
   else {size = 150 - count;}
   ellipse(width / 2, height / 2, size);
+}
+
+function keyPressed(){
+  if(keyIsDown(" ".charCodeAt(0))){
+    speed = (speed + 1) % 2;
+  }
 }
