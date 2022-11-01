4164393969410b64a501089ac0db299f2bb93555
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-11-01 11:36:39 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 100);
 }
 
-function balloon(t){
+function balloon(t, color){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 2; // これはすき間(padding)
+  fill(color);
+  noStroke();
+  rect(10, 10, w + p * 2, h + p * 2);
+  triangle(0, 15, 10, 20, 10, 10);
   fill(255);
-  text(t, p, h + p);
+  text(t, p + 10, h + p);
 }
