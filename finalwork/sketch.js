c84f83fa92f32abd16cbb318fe1205cbff7d6dd1
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-11-15 12:10:04 +0900

    Change1 finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -2,10 +2,17 @@
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  centx = width / 2;
+  centy = height / 2;
 }
 
 function draw(){
+  let p = 10; //すき間の設定
   background(160, 192, 255);
+  rect(centx - 400, 100, 800, 100);
+  textSize(30);
+  textAlign(CENTER);
+  text("ITコミュ", centx, 150);
 }
 
 function windowResized(){
