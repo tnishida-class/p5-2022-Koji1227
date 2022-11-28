405c48f8044590ca65af6a28cd6ff17e86c6e837
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-11-28 16:08:34 +0900

    Change 13 finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -21,6 +21,7 @@ let takencluster = [];
 let funlec = [];
 let funprof = [];
 let funcluster = [];
+
 let redlist = [];
 let bluelist = [];
 let cl;
@@ -36,7 +37,7 @@ function setup(){
   centy = height / 2;
   count1 = 0;
   count2 = 0;
-  speed = 5; // 落ちてくるスピードを調整
+  speed = 8; // 落ちてくるスピードを調整
   i1 = 0;
   i2 = 0;
   textcolor1 = 'black';
@@ -47,15 +48,12 @@ function setup(){
 }
 
 function draw(){
-  // console.log(takenlec);
   if(stage == 1.0){
-    // ステージ1・開始前
     background(160, 192, 255);
     frame('red');
     explanation();
   }
   if(stage == 1.1){
-    // ステージ1・冒頭
     background(160, 192, 255);
     frame('red');
     explanation();
@@ -66,7 +64,6 @@ function draw(){
     }
   }
   if(stage == 1.2){
-    // ステージ1・メイン
     count1 = count1 + speed;
     count2 = count2 + speed;
     if(count1 > height){
@@ -84,24 +81,16 @@ function draw(){
     explanation();
     box1(i1, textcolor1);
     box2(i2, textcolor2);
-    
-    // チェック！
-    // console.log(takenlec);
-  
     if(i1 * 2 >= leclist.length || i2 * 2 + 1 >= leclist.length){
       stage = 1.3
     }
   }
   if(stage == 1.3){
-    // ステージ1・終盤
     background(160, 192, 255);
     frame('red');
     explanation();
     if(i1 * 2 >= leclist.length){
-      // count1 = count1 + speed;
       count2 = count2 + speed;
-      // count1 = count1 % newheight;
-      // count2 = count2 % newheight;
       box2(i2, textcolor2);
       if(count2 >= newheight){
         stage = 2.0;
@@ -109,9 +98,6 @@ function draw(){
     }
     if(i2 * 2 + 1 >= leclist.length){
       count1 = count1 + speed;
-      // count2 = count2 + speed;
-      // count1 = count1 % newheight;
-      // count2 = count2 % newheight;
       box1(i1, textcolor1);
       if(count1 >= newheight){
         stage = 2.0;
@@ -124,7 +110,6 @@ function draw(){
     explanation();
   }
   if(stage == 2.1){
-    // ステージ2・冒頭
     background(160, 192, 255);
     frame('blue');
     explanation();
@@ -135,7 +120,6 @@ function draw(){
     }
   }
   if(stage == 2.2){
-    // ステージ2・メイン
     count1 = count1 + speed;
     count2 = count2 + speed;
     if(count1 > height){
@@ -153,16 +137,11 @@ function draw(){
     explanation();
     box1(i1, textcolor1);
     box2(i2, textcolor2);
-    
-    // チェック！
-    // console.log(funlec);
-  
     if(i1 * 2 >= takenlec.length || i2 * 2 + 1 >= takenlec.length){
       stage = 2.3
     }
   }
   if(stage == 2.3){
-    // ステージ2・終盤
     background(160, 192, 255);
     frame('blue');
     explanation();
@@ -182,12 +161,10 @@ function draw(){
     }
   }
   if(stage == 3.0){
-    // ここに診断結果を表示
-    // console.log(redlist);
     background(160, 192, 255);
-    for(let t = 0; t < 12; t++){
-      redlist[t] = 0;
-      bluelist[t] = 0;
+    for(let i = 0; i < 12; i++){
+      redlist[i] = 0;
+      bluelist[i] = 0;
     }
     for(let p = 0; p < takencluster.length; p++){
       for(let q = 0; q < 12; q++){
@@ -254,26 +231,26 @@ function explanation(){
     fill('red');
     textSize(20);
     textAlign(CENTER);
-    text("エンターを押すと開始します。", centx, 100);
+    text('エンターを押すと開始します。', centx, 100);
   }
   if(stage == 1.0 || stage == 1.1 || stage == 1.2 || stage == 1.3){
     fill('red');
     textSize(20);
     textAlign(CENTER);
-    text("自分の受けた授業が赤の枠内に来たらスペースを押してください。", centx, 50);
+    text('自分の受けた授業が赤の枠内に来たらスペースを押してください。', centx, 50);
   }
   if(stage == 2.0){
     fill('blue');
     textSize(20);
     textAlign(CENTER);
-    text("エンターを押すと開始します。", centx, 100);
+    text('エンターを押すと開始します。', centx, 100);
   }
   if(stage == 2.0 || stage == 2.1 || stage == 2.2 || stage == 2.3){
     stroke('blue');
     fill('blue');
     textSize(20);
     textAlign(CENTER);
-    text("次は，受けた授業のうち，面白かった授業が青の枠内に来たらスペースを押してください。", centx, 50);
+    text('次は，受けた授業のうち，面白かった授業が青の枠内に来たらスペースを押してください。', centx, 50);
   }
   if(stage == 3.0){
     // let list;
@@ -293,7 +270,6 @@ function explanation(){
 
     let x;
     let y;
-    // const cl = ['日本学', 'ｱｼﾞｱ・太平洋文化論', 'ﾖｰﾛｯﾊﾟ・ｱﾒﾘｶ文化論', '異文化関係論', '多文化共生論', '越境文化論', 'ﾓﾀﾞﾆﾃｨ論', '芸術文化論', '先端社会論', '言語ｺﾐｭﾆｹｰｼｮﾝ論', '感性ｺﾐｭﾆｹｰｼｮﾝ論', '情報ｺﾐｭﾆｹｰｼｮﾝ論'];
     for(i = 0; i < 12; i++){
       let theta = PI * 2 * i / 12 - HALF_PI;
       x = centx + cos(theta) * 6 * 50;
@@ -319,10 +295,10 @@ function box1(n, textcolor){
   textSize(24);
   textAlign(CENTER);
   if(stage == 1.1 || stage == 1.2 || stage == 1.3){
-    hyoji = leclist[n * 2] + "\n（" + proflist[n * 2] + "先生）";
+    hyoji = leclist[n * 2] + '\n（' + proflist[n * 2] + '先生）';
   }
   if(stage == 2.1 || stage == 2.2 || stage == 2.3){
-    hyoji = takenlec[n * 2] + "\n（" + takenprof[n * 2] + "先生）";
+    hyoji = takenlec[n * 2] + '\n（' + takenprof[n * 2] + '先生）';
   }
   text(hyoji, centx, count1 + 100);
 }
@@ -338,48 +314,32 @@ function box2(n, textcolor){
   textSize(24);
   textAlign(CENTER);
   if(stage == 1.1 || stage == 1.2 || stage == 1.3){
-    hyoji = leclist[n * 2 + 1] + "\n（" + proflist[n * 2 + 1] + "先生）";
+    hyoji = leclist[n * 2 + 1] + '\n（' + proflist[n * 2 + 1] + '先生）';
   }
   if(stage == 2.1 || stage == 2.2 || stage == 2.3){
-    hyoji = takenlec[n * 2 + 1] + "\n（" + takenprof[n * 2 + 1] + "先生）";
+    hyoji = takenlec[n * 2 + 1] + '\n（' + takenprof[n * 2 + 1] + '先生）';
   }
   text(hyoji, centx, count2 + 100);
 }
 
 function keyPressed(){
-  if(keyCode == " ".charCodeAt(0)){
+  if(keyCode == ' '.charCodeAt(0)){
     if(stage == 1.1 || stage == 1.2 || stage == 1.3){
       if(count1 >= centy - 150 && count1 <= centy + 200){
-        if(textcolor1 = 'red'){
-          // もう赤色になっている場合は元に戻す
-          // space_push(1, i1, 0, 1);
-        }
         // 色を赤に変えて「受けた授業リスト」にプッシュ
         space_push(1, i1, 'i1');
       }
       if(count2 >= centy - 150 && count2 <= centy + 200){
-        if(textcolor2 = 'red'){
-          // もう赤色になっている場合は元に戻す
-          // space_push(1, 0, i2, 1);
-        }
         // 色を赤に変えて「受けた授業リスト」にプッシュ
         space_push(1, i2, 'i2');
       }
     }
     if(stage == 2.1 || stage == 2.2 || stage == 2.3){
       if(count1 >= centy - 150 && count1 <= centy + 200){
-        if(textcolor1 = 'blue'){
-          // もう青色になっている場合は元に戻す
-          // space_push(2, i1, 0, 1);
-        }
         // 色を青に変えて「面白かった授業リスト」にプッシュ
         space_push(2, i1, 'i1');
       }
       if(count2 >= centy - 150 && count2 <= centy + 200){
-        if(textcolor2 = 'blue'){
-          // もう青色になっている場合は元に戻す
-          // space_push(2, 0, i2, 1);
-        }
         // 色を青に変えて「面白かった授業リスト」にプッシュ
         space_push(2, i2, 'i2');
       }
@@ -405,14 +365,6 @@ function space_push(a, b, c){
   // a : ステージ（整数値）, b : i1 or i2, c : i1かi2かの指標
   let color;
   let n;
-  // if(c == 0){
-  //   n = b * 2;
-  //   textcolor1 = color;
-  // }
-  // if(b == 0){
-  //   n = c * 2 + 1;
-  //   textcolor2 = color;
-  // }
   if(a == 1){
     color = 'red';
     if (c == 'i1') {
@@ -441,25 +393,13 @@ function space_push(a, b, c){
     funprof.push(takenprof[n]);
     funcluster.push(takencluster[n]);
   }
-  // if(d == 1){
-  //   if(a == 1){
-  //     //takenlec.pop();
-  //     takenprof.pop();
-  //     takencluster.pop();
-  //   }
-  //   if(a == 2){
-  //     funlec.pop();
-  //     funprof.pop();
-  //     funcluster.pop();
-  //   }
-  // }
 }
 
 function regularPolygon(cx, cy, r, list, color){
   let n = list.length;
   let newlist = [];
-  for(j = 0; j < n; j++){
-    newlist.push(list[j] * 50);
+  for(i = 0; i < n; i++){
+    newlist.push(list[i] * 50);
   }
   for(i = 0; i < n; i++){
     let theta = TWO_PI * i * 1 / n - HALF_PI;
@@ -481,8 +421,10 @@ function regularPolygon(cx, cy, r, list, color){
   }
 }
 
+// 講義科目のリストアップ
 function listup(){
   leclist = ['日本社会文化論', '日本思想文化論', '日本文化交流論','日本メディア文化論', '日本歴史文化論', '東アジア政治社会論', 'オセアニア社会文化論', '北アジア歴史社会論', '東南アジア社会文化論', '東南アジア政治文化論', '環大西洋文化論', 'アメリカ社会論', 'アメリカ文化論', '英米テクスト文化論', '宗教文化論', 'ヨーロッパ社会文化論', '文化人類学', '現代社会人類学', '現代民族誌学', '比較民族学', '文化混交論', '越境文化形成論', '科学技術文明論', '比較文化論', '文化翻訳論', '越境社会文化論', '国際関係論', '多文化政治社会論', '比較政策論', '比較政治社会論', '平和構築論', '近現代社会思想論', '近現代文化言説論', '近現代表象文化論', '近現代経済思想論', '近現代政治思想論', '現代社会理論', 'グローバル正義論', 'ジェンダー社会文化論', 'メディア社会文化論', '現代規範論', '文化政策論', '近現代アート論', '芸術文化表象論', '視覚文化論', '表象文化形成論', '芸術文化環境論', '第二言語習得論', '言語機能論', 'コミュニケーション表現論', '翻訳コミュニケーション論', 'グローバル・イングリッシュ・ヒストリー', '非言語コミュニケーション論', '音声コミュニケーション論', 'コミュニケーション構造論', 'コミュニケーション比較論', '認知コミュニケーション論', '現代IT入門', 'ITコミュニケーションデザイン', '社会システム科学', 'データマネージメント', '統計情報処理'];
+  // 2022年度の担当教員/2021年度の担当教員
   proflist = ['辛島/栢木', '昆野', '寺内', '板倉', '長', '谷川', '深川/平野', '池尻/萩原', '伊藤', '貞好', '小澤', '井上', '西谷', '松家・西谷・深町/野谷', '野谷', '衣笠/坂本', '大石', '石田(慎)/梅屋', '下條', '岡田', '齋藤', '田中(祐)','塚原', '深町/遠田', '北村', '近藤', '宮脇/坂井', '新川', '安岡', '李/阪野', '中村', '鹿野', '石田(圭)', '松家', '市田', '上野', '西澤','櫻井', '青山', '小笠原', '工藤', '藤野', '池上', '上畑/岡本', '蟻谷/井口', '岩本', '高田', '田中(順)', '石田(雄)', '小松原', '藤濤', '西村', '北田', '水口', '南本', '巽', '松本', '大月/森下', '西田', '村尾', '清光', '康'];
   for(let x = 0; x < 12; x++){
     for(let y = 0; y < 5; y++){
@@ -494,7 +436,7 @@ function listup(){
       clusterlist.push(x);
     }
   }
-  for(let t = 0; t < 12; t++){
+  for(let z = 0; z < 12; z++){
     redlist.push(0);
     bluelist.push(0);  
   }
