a251a3470dce5ea48223e19dacced7e18f273fe7
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-12-31 03:08:07 +0900

    Change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -7,7 +7,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(20, 100); // 20以上100未満のランダムな数を代入
   }
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
@@ -15,4 +15,11 @@ function setup(){
   for(let i = 0; i < scores.length; i++){ total += scores[i]; }
 
   // BLANK[1]
+  let ratio = [];
+  let sum = 0;
+  for(let i = 0; i < scores.length; i++){
+    ratio[i] = scores[i] / total;
+    sum += ratio[i];
+    arc(200, 200, 300, 300, (sum - (ratio[i])) * TWO_PI, sum * TWO_PI, PIE);
+  }
 }
