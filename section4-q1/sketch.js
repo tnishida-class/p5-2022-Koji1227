9f6f3ecb31b10b466dfabe8414f6553ff01f9106
Koji1227 <115434297+Koji1227@users.noreply.github.com>
2022-12-31 20:01:59 +0900

    Change section4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -14,7 +14,7 @@ function setup(){
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
 
-  console.log(scores);
+  // console.log(scores);
 
   // ここからが本番
   fill(0);
@@ -26,12 +26,21 @@ function setup(){
     circle(dx * (i + 0.5), ( 100 - scores[i]) / 100 * height, 7);
   }
   px = dx * 0.5;
-  py = ( 100 - scores[0]) / 100 * height;
+  py = (100 - scores[0]) / 100 * height;
   for(let i = 1; i < scores.length; i ++){
     new_px = dx * (i + 0.5);
     new_py = ( 100 - scores[i]) / 100 * height;
     line(px, py, new_px, new_py);
     px = new_px;
     py = new_py;
+  }
+}
+
+function draw(){
+  console.log(mouseX, mouseY);
+  for(let i = 0; i < scores.length; i++){
+    if(dx * (i + 0.5) - 5 < mouseX && mouseX < dx * (i + 0.5) + 5 && ( 100 - scores[i]) / 100 * height - 5 < mouseY && mouseY < ( 100 - scores[i]) / 100 * height + 5){
+      text(scores[i], dx * (i + 0.5), ( 100 - scores[i]) / 100 * height + 10);
+    }
   }
 }
